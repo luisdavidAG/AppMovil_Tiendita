9d3a045bead36927702b5ba8bb57b386cb0960e4
luisdavidAG <alcozergonzalezluisdavid@gmail.com>
2024-10-30 15:19:52 -0600

    Commit Luis David Lap -rediseño feddback tu inventario

--- tiendita/lib/main.dart
+++ tiendita/lib/main.dart
@@ -30,7 +30,7 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       debugShowCheckedModeBanner: false,
-      home: const loginScrenn(),
+      home: const TuInventario_Screen(),
     );
   }
 }
--- tiendita/lib/widgets/Personalizados_Screnns/FeedBack_Inventario.dart
+++ tiendita/lib/widgets/Personalizados_Screnns/FeedBack_Inventario.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class FBInventario extends StatefulWidget {
   const FBInventario({super.key});
@@ -6,74 +7,120 @@ class FBInventario extends StatefulWidget {
   @override
   State<FBInventario> createState() => _FBInventarioState();
 }
-//Final Variables de las cosas que se muestran
-//Aqui se mandan llamar las consultas de lo que quiero mostrar
-//Se muestras en variables
-//dentro del contenedor
-//TODO alo mejor como no es interactivo cambiar esto STALEESS
 
 class _FBInventarioState extends State<FBInventario> {
   final double padings_H = 40;
   final double padings_V = 25;
+  late String formattedDate;
+
+  @override
+  void initState() {
+    super.initState();
+    // Obtenemos la fecha actual y la formateamos
+    DateTime now = DateTime.now();
+    formattedDate = DateFormat('EEEE, d MMMM yyyy', 'es_ES').format(now); // "viernes, 8 de mayo de 2023"
+  }
 
   @override
   Widget build(BuildContext context) {
     Size pantalla = MediaQuery.of(context).size;
 
-    return Container(
-     // height: pantalla.height*.40,
-      width: pantalla.width,
+    return Scaffold(
+      backgroundColor: Colors.white, // Fondo blanco
+      body: Container(
+        width: pantalla.width,
 
-      //DECORACIONES
-      decoration: BoxDecoration(
-        color: Colors.greenAccent,
-        borderRadius: BorderRadius.only(topLeft: Radius.circular(18),bottomRight: Radius.circular(18), bottomLeft: Radius.elliptical(10,6),topRight: Radius.elliptical(10,6))
-      ),
+        // DECORACIONES
+        decoration: BoxDecoration(
+          color: Colors.blueAccent[100], // Cambié el color para ver el contenedor
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(18),
+            bottomRight: Radius.circular(18),
+            bottomLeft: Radius.elliptical(10, 6),
+            topRight: Radius.elliptical(10, 6),
+          ),
+        ),
 
-      //Contenido
-      child: Column(
-        children: [
-          Container(
-            margin: EdgeInsets.symmetric(horizontal:padings_H,vertical: 15),
-            alignment: Alignment.centerLeft,
-            child: Text('Detalles')),
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: padings_H, vertical:5 ),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                //Titulo
-                Text('Tu inventario'),
-                //Contenido que cambia XD
-                Text('Num_Productos')
-              ],
+        // Contenido
+        child: Column(
+          children: [
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: padings_H),
+              child: Container(
+                height: pantalla.height * .10,
+                width: pantalla.width,
+                alignment: Alignment.centerLeft,
+                child: const Padding(
+                  padding: EdgeInsets.only(left: 5),
+                  child: CircleAvatar(
+                    maxRadius: 25,
+                    backgroundColor: Colors.white,
+                  ),
+                ),
+              ),
             ),
-          ),
-           Padding(
-            padding: EdgeInsets.symmetric(horizontal: padings_H, vertical:5 ),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                //Titulo
-                Text('Tu inventario'),
-                //Contenido que cambia XD
-                Text('Num_Productos')
-              ],
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: padings_H),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  const Text(
+                    'Tu Tienda',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 10.0),
+                    child: Text(formattedDate), // Mostramos la fecha actual
+                  ),
+                ],
+              ),
             ),
-          ),
-           Padding(
-            padding: EdgeInsets.symmetric(horizontal: padings_H, vertical:5 ),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                //Titulo
-                Text('Tu inventario'),
-                //Contenido que cambia XD
-                Text('Num_Productos')
-              ],
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: padings_H, vertical: 15),
+              alignment: Alignment.centerLeft,
+              child: const Text(
+                'Detalles de \nTu Inventario',
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
             ),
-          ),
-        ],
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: padings_H, vertical: 5),
+              child: const Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text('Tu inventario'),
+                  Text('Num_Productos'),
+                ],
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: padings_H, vertical: 5),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: const [
+                  Text('Tu inventario'),
+                  Text('Num_Productos'),
+                ],
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: padings_H, vertical: 5),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: const [
+                  Text('Tu inventario'),
+                  Text('Num_Productos'),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
